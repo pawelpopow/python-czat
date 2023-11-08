8139b3bbad8745e1c5d1ac8eaca0243be9ed0f32
ppopow <pawel.popow@gmail.com>
2023-11-08 16:37:05 +0000

    three commit

--- czat/models.py
+++ czat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+
+    """Klasa reprezentująca wiadomość w systemie"""
+    text = models.CharField('message content', max_length=250)
+    data_pub = models.DateTimeField('date of publication', auto_now_add=True)
+    author = models.ForeignKey(User)
+
