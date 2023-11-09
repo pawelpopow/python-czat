724cd12c0db32f5326e47c2706fcdc3f845c1030
ppopow <pawel.popow@gmail.com>
2023-11-09 10:16:45 +0000

    five commit

--- czat/models.py
+++ czat/models.py
@@ -9,3 +9,11 @@ class Message(models.Model):
     data_pub = models.DateTimeField('date of publication', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = u'the news'
+        verbose_name_plural = u'the news'
+        ordering = ['data_pub']
+
+    def __str__(self):
+        return self.text
+
