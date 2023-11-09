a6f104ebfae2118ccd4ef482b4c6866ff4a2e545
ppopow <pawel.popow@gmail.com>
2023-11-09 12:27:50 +0000

    seven commit

--- czat/views.py
+++ czat/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
     """Strona główna aplikacji."""
-    return HttpResponse("Witaj w aplikacji Czat!")
+    # return HttpResponse("Witaj w aplikacji Czat!")
+    return render(request, 'czat/index.html')
